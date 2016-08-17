1639032273f5e4b725d479cda95bd1d33700b286
Jon Kamke <jon@kamke.org>
2016-08-17 02:46:00 -0500

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularDrlubeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularDrlubeApp
+ */
+angular.module('angularDrlubeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
