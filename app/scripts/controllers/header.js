7b19d70687c9cf2d21fc1eb5cffbd59bea621b81
Jon Kamke <jon@kamke.org>
2016-08-23 19:10:15 -0500

    Remove hard coded uuid

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -11,10 +11,6 @@ angular.module('angularDrlubeApp')
   .controller('HeaderCtrl', function ($scope, $location, $http) {
     $scope.mainMenu = [];
 
-    $scope.homeUUID = '4a2b35b7-e8fc-4798-9b5a-41c175838381';
-    $scope.servicesUUID = '4a2b35b7-e8fc-4798-9b5a-41c175838381';
-    $scope.contactUUID = '4a2b35b7-e8fc-4798-9b5a-41c175838381';
-
     var path = 'http://api.drlube.dev/v1/menu';
 
     $http.get(path).then(function(result) {
